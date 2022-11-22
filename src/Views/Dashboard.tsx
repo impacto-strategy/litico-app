51b1fdbb8846b459ddda9e7fb7714522e0639313
mattadam <mattnery@gmail.com>
2022-11-22 09:32:32 -0700

    fixes GHG with different date formats

--- src/Views/Dashboard.tsx
+++ src/Views/Dashboard.tsx
@@ -1,7 +1,7 @@
 /* IMPORT EXTERNAL MODULES */
 import { FC, useCallback, useEffect, useMemo, useState } from "react";
 import {Divider} from "antd";
-import {filter, find, flatten, forOwn, groupBy, isEmpty, map, sortBy, sumBy} from "lodash";
+import {filter, find, flatten, forOwn, groupBy, isEmpty, map, sortBy, sumBy, uniq} from "lodash";
 
 /* IMPORT INTERNAL MODULES */
 // UNUSED COMPONENTS
@@ -30,6 +30,7 @@ import ResourceService from "../Services/ResourceService";
 import useAuth from "../Providers/Auth/useAuth";
 import {ArrOfObj} from "../../global"
 import { extractYear } from "../utils";
+import { debug } from "console";
 
 const Dashboard: FC = () => {
     // React State
@@ -80,6 +81,21 @@ const Dashboard: FC = () => {
         return sortBy(flatten(map(data, (m: any) => ([
             { date: m.date, value: m.value }
         ]))), ['year'])
+    }, [metrics])
+
+    const getMetricsByYear = useCallback((year: number, basin: string, metricSubtype: string) => {
+        let data = filter(metrics.esg_metrics, { 'metric_subtype': metricSubtype })
+
+        let metricByYear = find(data, (em) => {
+            if (basin) {
+                return new Date(em.date).getFullYear() === year && em.basin === basin
+            } else {
+                return new Date(em.date).getFullYear() === year
+            }
+        })
+
+        if (!metricByYear?.value) return null
+        return metricByYear
     }, [metrics])
 
     const getIntensityByDate = useCallback((date: string, basin: string, intensityType: string) => {
@@ -160,23 +176,26 @@ const Dashboard: FC = () => {
     }, [metrics])
 
     const getYearlyEmissionData = useMemo(() => {
-        return flatten(map(sortBy(groupBy(emissions, 'date'), 'date'), (e: ArrOfObj) => {
-            let data = []
-            for (let i = 0; i < e.length; i++) {
-                let intensity = getIntensityByDate(e[i].date, e[i].basin, 'ghg')
+        let dates = uniq(map(emissions, ((metric) => new Date(metric.date).getFullYear()))).sort()
+        let basins = uniq(map(emissions, 'basin')).sort().filter(i => typeof i === 'string');
+        return flatten(map(dates, ((date) => {
+            return flatten(map(basins, ((basin) => {
+                let data = []
+                let ghg = getMetricsByYear(date, basin, 'GHG Emissions')
+                let intensity = getMetricsByYear(date, basin, 'GHG Intensity - BOE')
                 data.push({ 
-                    name: "GHG Emissions (CO2e)", 
-                    type: parseInt(e[i].date), 
-                    value: e[i].value, 
-                    intensity: intensity,
-                    basin: e[i].basin, 
+                    name: "GHG Emissions (CO2e)",
+                    type: date,
+                    value: ghg?.value || 0,
+                    intensity: intensity?.value || 0,
+                    basin: basin,
                     // Utilized to avoid bugs in GHG Chart
-                    label: `${e[i].basin} Emissions Intensity (mt/BoE)` 
+                    label: `${basin} Emissions Intensity (mt/BoE)`
                 })
-            }
-            return data
-        }))
-    }, [emissions, getIntensityByDate])
+                return data
+            })))
+        })))
+    }, [emissions, getMetricsByYear])
 
     const getYearlyComplaintsData = useMemo(() => {
         return flatten(map([2017, 2018, 2019, 2020, 2021, 2022], (e) => {
