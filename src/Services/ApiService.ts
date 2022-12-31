2602cc141d9d895821eb6afd92f4a8f32baa75cf
rsato1007 <robertmsato@gmail.com>
2022-12-30 19:38:00 -0700

    remove api port number

--- src/Services/ApiService.ts
+++ src/Services/ApiService.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 import AuthService from "./AuthService";
 
 const APIClient = axios.create({
-    baseURL: process.env.API_URL || 'http://localhost:8000',
+    baseURL: process.env.API_URL || 'http://localhost',
     withCredentials: true, // required to handle the CSRF token
 });
 
