113f1c774ad27ea999bb2d47e6f344504e955003
rsato1007 <robertmsato@gmail.com>
2023-07-06 16:26:54 -0600

    removing webpack config and merging with craco config

--- craco.config.js
+++ craco.config.js
@@ -2,29 +2,48 @@ const CracoLessPlugin = require('craco-less');
 const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-    plugins: [
-        {
-            plugin: CracoLessPlugin,
-            options: {
-                lessLoaderOptions: {
-                    lessOptions: {
-                        javascriptEnabled: true,
-                    },
-                },
-            },
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            javascriptEnabled: true,
+          },
         },
-    ],
-    webpack: {
-        plugins: [
-            new Dotenv({
-                systemvars: true
-            })
-        ],
-        configure: webpackConfig => {
-            return {
-                ...webpackConfig,
-                ignoreWarnings: [/Failed to parse source map/],
-            }
-        },
+      },
     },
+  ],
+  webpack: {
+    module: {
+      rules: [
+        {
+          test: /\.less$/i,
+          use: [
+            "style-loader",
+            "css-loader",
+            {
+              loader: 'less-loader',
+              options: {
+                lessOptions: {
+                  javascriptEnabled: true,
+                },
+              },
+            },
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new Dotenv({
+        systemvars: true,
+      }),
+    ],
+    configure: (webpackConfig) => {
+      return {
+        ...webpackConfig,
+        ignoreWarnings: [/Failed to parse source map/],
+      };
+    },
+  },
 };
